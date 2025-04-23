d3a87d64b35ac8a4a0ed61615891484ace57370d
tatya1213 <tatya3591@gmail.com>
2025-04-23 14:04:22 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,7 @@
+x = 5
+y = 3
+print(f"x = {x}, y = {y}")
+print("\nLogical Results:")
+print(f"AND (x > 0 and y < 5): {x > 0 and y < 5}")
+print(f"OR (x == 5 or y == 10): {x == 5 or y == 10}")
+print(f"NOT (not x == 5): {not x == 5}")
