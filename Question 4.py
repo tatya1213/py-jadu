83eff098e3ba32b38b2053ff4bc1dbb4f3aa667a
tatya1213 <tatya3591@gmail.com>
2025-04-23 14:06:34 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,8 @@
+x = 10 # 1010
+y = 4 # 0100
+print("Bitwise AND:", x & y)
+print("Bitwise OR:", x | y)
+print("Bitwise NOT:", ~x)
+print("Bitwise XOR:", x ^ y)
+print("Right Shift:", x >> 1)
+print("Left Shift:", x << 1)
