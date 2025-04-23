206ed8a197f968241a2702adc7814ed0205ea9dc
tatya1213 <tatya3591@gmail.com>
2025-04-23 22:02:13 +0530

    Create question 9 and 18.py

---
+++ question 9 and 18.py
@@ -0,0 +1,21 @@
+# Part 1
+password1 = "admin123"
+password2 = "admin123"
+if password1 == password2:
+    print("Password verified")
+else:
+    print("Password mismatch")
+
+# Part 2
+num = 7
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Part 3
+name = "Alice"
+if name == "Alice":
+    print("Hello Alice!")
+else:
+    print("Hello!")
