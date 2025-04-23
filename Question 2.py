aba64b7ac4a8421c5a05e5bdef0098b2149df66b
tatya1213 <tatya3591@gmail.com>
2025-04-23 13:47:42 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+print("x =", x, "y =", y)
+print("Equal:", x == y)
+print("Not Equal:", x != y)
+print("Greater Than:", x > y)
+print("Less Than:", x < y)
+print("Greater Than Equal:", x >= y)
+print("Less Than Equal:", x <= y)
